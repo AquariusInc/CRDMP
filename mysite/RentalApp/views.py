6d7c43d6cb070f4769edf6afae58c5b04b105021
Aidan Kinzett <aidan@aidankinzett.com>
2018-09-03 16:53:59 +1000

    added templates and css

--- mysite/RentalApp/views.py
+++ mysite/RentalApp/views.py
@@ -3,5 +3,26 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def testApp(request):
-    return render(request, 'test.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def customers_table(request):
+    return render(request, 'customers_table.html')
+
+
+def rental_table(request):
+    return render(request, 'rental_table.html')
+
+
+def customer_data(request):
+    return render(request, 'visualise_customer_data.html')
+
+
+def rental_data(request):
+    return render(request, 'visualise_rental_data.html')
+
+
+def vehicle_data(request):
+    return render(request, 'visualise_vehicle_data.html')
