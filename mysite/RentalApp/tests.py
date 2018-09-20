886f84dfb5ea732ea22ac3fda1e234d1b8424b0a
JoshT <josh.tovas@connect.qut.edu.au>
2018-09-20 17:53:31 +1000

    fixed errors preventing testing from working and minor js code bug fix

--- mysite/RentalApp/tests.py
+++ mysite/RentalApp/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class visualise_vehicle_data_tests(TestCase):
+    def test_dicks(self):
+        tuesday = 5
+        self.assertIs(tuesday, 5)
